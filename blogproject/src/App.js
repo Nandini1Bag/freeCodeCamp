46728da8bfb93f66d3c3e00986bfb8ab5613d992
nandini <nininandini@gmail.com>
2020-08-26 15:48:06 -0700

    Change Name of files.

--- blogproject/src/App.js
+++ blogproject/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Home from './containers/Home';
+import Home from './containers/Home/Home';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
